d9e9106b4d3da768a1cb460880a11a5c4ae787ba
Andris Rožkalns <277435981+rozkalnsandris@users.noreply.github.com>
2026-08-15 23:40:36 +0200

    fix: remove obsolete placeholder route import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
-import { AppShell, DockerPage, PlaceholderPage, RouteErrorPage } from "./App";
+import { AppShell, DockerPage, RouteErrorPage } from "./App";
 import { ActivityPage } from "./pages/ActivityPage";
 import { BackupsPage } from "./pages/BackupsPage";
 import { DeploymentsPage } from "./pages/DeploymentsPage";
