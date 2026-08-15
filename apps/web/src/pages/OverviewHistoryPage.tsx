a3988ca747c08942baa97dbe2c0c67ef00acf222
Andris Rožkalns <277435981+rozkalnsandris@users.noreply.github.com>
2026-08-15 22:51:49 +0200

    feat: add backup health to overview

--- apps/web/src/pages/OverviewHistoryPage.tsx
+++ apps/web/src/pages/OverviewHistoryPage.tsx
@@ -1,10 +1,12 @@
 import { OverviewPage } from "../App";
+import { BackupOverviewStatus } from "./BackupOverviewStatus";
 import { HistoryPanel } from "./HistoryPanel";
 
 export function OverviewHistoryPage() {
   return (
     <div className="overview-composed">
       <OverviewPage />
+      <BackupOverviewStatus />
       <HistoryPanel />
     </div>
   );
